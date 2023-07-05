159928d736499bcc9725077dd4e0f4b6f0016464
raaizar <rahmanraiza@gmail.com>
2023-07-05 20:41:55 +0700

    json fetching done

--- app/src/main/java/com/example/convo_app/fragment_home.java
+++ app/src/main/java/com/example/convo_app/fragment_home.java
@@ -1,10 +1,9 @@
 package com.example.convo_app;
 
-import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
-
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,30 +15,37 @@ import android.view.ViewGroup;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.convo_app.adapters.post_adapter;
 import com.example.convo_app.models.post;
+import com.example.convo_app.utils.api_service;
 import com.example.convo_app.utils.post_database_helper;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class fragment_home extends Fragment {
     private View view;
     private Context context;
     private RecyclerView timelineRV;
     private post_database_helper postDb;
-    private RequestQueue requestQueue;
-    private String URL = "https://jsonplaceholder.typicode.com/posts";
+    private String URL = "https://jsonplaceholder.typicode.com/posts/";
     private post_adapter adapter;
     private List<post> posts;
+    private RequestQueue requestQueue;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -59,44 +65,36 @@ public class fragment_home extends Fragment {
         postDb = new post_database_helper(context);
         requestQueue = Volley.newRequestQueue(context);
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(URL, , new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                Log.i("sampe sini", "sampe sini");
-                try {
-                    JSONArray jsonArray = response.getJSONArray("posts");
-                    posts = new ArrayList<>();
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, URL, null,
+                response -> {
+                    try {
+                        posts = new ArrayList<>();
+                        Log.i("tes", "sampe sini");
 
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject jsonObject = response.getJSONObject(i);
 
-                        Integer userId = jsonObject.getInt("userId");
-                        Integer id = jsonObject.getInt("id");
-                        String title = jsonObject.getString("title");
-                        String body = jsonObject.getString("body");
+                            Integer id = jsonObject.getInt("id");
+                            Integer userId = jsonObject.getInt("userId");
+                            String title = jsonObject.getString("title");
+                            String body = jsonObject.getString("body");
+                            Log.i("asd", userId + title + body);
+                            insertToDatabase(userId, id, title, body);
+                            post post = new post(userId, id, title, body);
+                            posts.add(post);
+                        }
 
-                        Log.i("ASD", userId + id + title + body);
-
-                        insertToDatabase(userId, id, title, body);
-                        post newPost = new post(userId, id, title, body);
-                        posts.add(newPost);
                         setRecyclerview(posts, context);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
+                },
+                Throwable::printStackTrace
+        );
+        requestQueue.add(request);
 
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-
-        requestQueue.add(jsonObjectRequest);
     }
 
     private void setRecyclerview(List<post> posts, Context context) {
